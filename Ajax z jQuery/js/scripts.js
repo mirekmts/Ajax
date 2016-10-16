67f2df6660e9e67f8434dc41534a1cc80c39492c
Mirek <mirekmts@gmail.com>
2016-10-16 16:09:06 +0200

    Dodane css

--- Ajax z jQuery/js/scripts.js
+++ Ajax z jQuery/js/scripts.js
@@ -1,21 +1,29 @@
-var url = 'http://api.icndb.com/jokes/random';
-var $button = $('#get-joke').click(function() {
-	getJoke();
-});
-var $paragraph = $('#joke');
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "http://api.forismatic.com/api/1.0/?method=getQuote&key=867576&format=jsonp&lang=en&jsonp=?";
 
-function getJoke() {
-	$.ajax({
-		method: 'GET',
-		url: url,
-		success:function(res) {
-			$paragraph.text(res.value.joke);
-			//res.send();
-		}
-	});
+function getQuote() { 
+	$.getJSON(quoteUrl, createTweet); 
 }
 
+function createTweet(input) {
+	if(!input.quoteAuthor.length) {
+		input.quoteAuthor = "Unkown author";
+	}
+	
+	var tweetText = "Quote of the day - " + input.quoteText + " Author: " + input.quoteAuthor;
+		
+	if (tweetText.length > 140) { 
+		getQuote(); 
+	} else { 
+		var tweet = tweetLink + encodeURIComponent(tweetText); 
+		$('.quote').text(input.quoteText); 
+		$('.author').text("Author: " + input.quoteAuthor); 
+		$('.tweet').attr('href', tweet); }
+}
 
-
-
-
+$(document).ready(function() { 
+	getQuote(); 
+	$('.trigger').click(function() { 
+		getQuote(); 
+	}) 
+});
